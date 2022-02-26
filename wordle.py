c54e35f0a965ca12d1e412c01c25309884fa235f
Tirth-Patel903 <tirthp903@gmail.com>
2022-02-26 22:05:32 +0530

    Changes made for adding exception and IO Operations

--- wordle.py
+++ wordle.py
@@ -2,6 +2,17 @@ import os,sys,random
 import tkinter as tk
 from tkinter import simpledialog
 from tkinter import messagebox
+import logging
+from datetime import datetime
+
+logging.basicConfig(filename='GamePlay.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s')
+#Let us Create an object 
+logger=logging.getLogger() 
+
+#Now we are going to Set the threshold of logger to DEBUG 
+logger.setLevel(logging.DEBUG)
+todayDate=datetime.today().strftime("%m/%d/%Y, %H:%M:%S")
+logger.info('Your Gameplay for '+todayDate)
 
 staticals_list=[]
 def main():
@@ -25,7 +36,7 @@ def main():
         staticals_list.pop(0)
         staticals_list.pop(0)
         staticals_list.pop(0)
-
+        logger.info(messagebox_info)
         if playAgain == 'yes':
             total_game_played+=1
             messagebox.showinfo("Game Staticals","\n".join(messagebox_info))
@@ -39,6 +50,16 @@ def main():
         else:
             print("Goodbye")
 
+def word_fetch():
+    try:
+        a = open("demo.txt", "w")
+        f = open("words.txt", "r")
+        for x in f:
+            if(len(x)==6):
+                a.write(x)
+    except IOError:
+        print("File not accessible")
+    a.close()
 
 def processGuess(theAnswer, theGuess):
     position=0
@@ -57,7 +78,10 @@ def processGuess(theAnswer, theGuess):
     print(clue)
     staticals_list.append(distribution)
     print(staticals_list)
-    print(distribution)
+    logger.info('Your clue:-'+ clue)
+    logger.info('Answer:-'+ theAnswer)
+    logger.info('Your Guess:-'+ theGuess)
+    logger.info('Your guessing percentage:-'+str(distribution))
     return clue == "GGGGG" #True if correct,False otherwie
 
 def verifyGuess(theGuess):
@@ -66,9 +90,26 @@ def verifyGuess(theGuess):
     else:
         return theGuess
 
+def removeWord(answer):
+    #Code for removing word which is already selected To avoid repeated word in Gameplay.
+    wordList=open("demo.txt", "r+")
+    lines = wordList.readlines()
+    wordList.truncate(0)
+    for line in lines:
+        if line.strip() != answer:
+            wordList.write(line)
+        
+    wordList.close()
+
 def logicCode():
-    wordList=["about","above","abuse","actor","acute","admit","adopt","adult","after","again","agent","agree","ahead","alarm","album","alert","alike","alive","allow","alone","along","alter","among","anger","Angle","angry","apart","apple","apply","arena","argue","arise","array","aside","asset","audio","audit","avoid","award","aware","badly","baker","bases","basic","basis","beach","began","begin","begun","being","below","bench","billy","birth","black","blame","blind","block","blood","board","boost","booth","bound","brain","brand","bread","break","breed","brief","bring","broad","broke","brown","build","built","buyer","cable","calif","carry","catch","cause","chain","chair","chart","chase","cheap","check","chest","chief","child","china","chose","civil","claim","class","clean","clear","click","clock","close","coach","coast","could","count","court","cover","craft","crash","cream","crime","cross","crowd","crown","curve","cycle","daily","dance","dated","dealt","death","debut","delay","depth","doing","doubt","dozen","draft","drama","drawn","dream","dress","drill","drink","drive","drove","dying","eager","early","earth","eight","elite","empty","enemy","enjoy","enter","entry","equal","error","event","every","exact","exist","extra","faith","false","fault","fiber","field","fifth","fifty","fight","final","first","fixed","flash","fleet","floor","fluid","focus","force","forth","forty","forum","found","frame","frank","fraud","fresh","front","fruit","fully","funny","giant","given","glass","globe","going","grace","grade","grand","grant","grass","great","green","gross","group","grown","guard","guess","guest","guide","happy","harry","heart","heavy","hence","henry","horse","hotel","house","human","ideal","image","index","inner","input","issue","japan","jimmy","joint","jones","judge","known","label","large","laser","later","laugh","layer","learn","lease","least","leave","legal","level","lewis","light","limit","links","lives","local","logic","loose","lower","lucky","lunch","lying","magic","major","maker","march","maria","match","maybe","mayor","meant","media","metal","might","minor","minus","mixed","model","money","month","moral","motor","mount","mouse","mouth","movie","music","needs","never","newly","night","noise","north","noted","novel","nurse","occur","ocean","offer","often","order","other","ought","paint","panel","paper","party","peace","peter","phase","phone","photo","piece","pilot","pitch","place","plain","plane","plant","plate","point","pound","power","press","price","pride","prime","print","prior","prize","proof","proud","prove","queen","quick","quiet","quite","radio","raise","range","rapid","ratio","reach","ready","refer","right","rival","river","robin","roger","roman","rough","round","route","royal","rural","scale","scene","scope","score","sense","serve","seven","shall","shape","share","sharp","sheet","shelf","shell","shift","shirt","shock","shoot","short","shown","sight","since","sixth","sixty","sized","skill","sleep","slide","small","smart","smile","smith","smoke","solid","solve","sorry","sound","south","space","spare","speak","speed","spend","spent","split","spoke","sport","staff","stage","stake","stand","start","state","steam","steel","stick","still","stock","stone","stood","store","storm","story","strip","stuck","study","stuff","style","sugar","suite","super","sweet","table","taken","taste","taxes","teach","teeth","terry","texas","thank","theft","their","theme","there","these","thick","thing","think","third","those","three","threw","throw","tight","times","tired","title","today","topic","total","touch","tough","tower","track","trade","train","treat","trend","trial","tried","tries","truck","truly","trust","truth","twice","under","undue","union","unity","until","upper","upset","urban","usage","usual","valid","value","video","virus","visit","vital","voice","waste","watch","water","wheel","where","which","while","white","whole","whose","woman","women","world","worry","worse","worst","worth","would","wound","write","wrong","wrote","yield","young","youth"]
-    answer=random.choice(wordList)
+    p=os.path.getsize("demo.txt")
+    if(p==0):
+        word_fetch()
+        
+    wordList=open("demo.txt", "r+")
+    words = wordList.read().splitlines()
+    answer=random.choice(words)
+    removeWord(answer)
     num_of_guesses=0
     guesses_correctly = False
     print("G=Letter is True, Y=True Letter In wrong Spot and _= Wrong Letter")
@@ -76,13 +117,12 @@ def logicCode():
         #get input from user
         ROOT = tk.Tk()
         ROOT.withdraw()
-        # the input dialog
+        # the input dialog         
         guess = simpledialog.askstring(title="Input Box",prompt="Enter five character:")
         theGuess=verifyGuess(guess)
         #guess = input("Enter five character:")
         print("You have guess",theGuess)
         num_of_guesses += 1
-
         #process guess
         guessed_correctly = processGuess(answer, theGuess)      
     return guessed_correctly,num_of_guesses
